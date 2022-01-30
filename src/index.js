4f68d07cff10e1f3a7604767089e2af8826adb38
Wifsimster <wifsimster@gmail.com>
2022-01-30 13:55:14 +0100

    fix service

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ async function main() {
   })
 
   try {
-    await fastify.listen(PORT)
+    await fastify.listen(PORT, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
