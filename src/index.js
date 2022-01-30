0ea47e4630dd0e8d23ee7862fb7bf5f5dc4bba96
Wifsimster <wifsimster@gmail.com>
2022-01-30 11:14:27 +0100

    fix: update routes

--- src/index.js
+++ src/index.js
@@ -7,21 +7,35 @@ const mongoUrl = `mongodb://${config.mongo.host}:${config.mongo.port}/`
 const PORT = 3001
 const mongoClient = new MongoClient(mongoUrl)
 
-fastify.register(require('fastify-cors'), {
-  // put your options here
-})
+fastify.register(require('fastify-cors'), {})
 
 fastify.get('/linky', async (request, reply) => {
   try {
     const db = mongoClient.db(config.mongo.db)
     const linky = db.collection('linky')
+    const cursor = linky.find().sort({ _id: -1 }).limit(1)
+    let results = await cursor.toArray()
+    return results[0]
+  } catch (err) {
+    return err
+  }
+})
+
+fastify.get('/teleinfo', async (request, reply) => {
+  try {
+    const db = mongoClient.db(config.mongo.db)
+    const linky = db.collection('linky')
+    let sort = {}
+    sort[request.query.sort] = request.query.order
     const options = {
-      sort: { createdAt: -1 },
+      sort,
       projection: {
         createdAt: 1,
-        index: 1,
+        instantaneousIntensity01: 1,
+        instantaneousIntensity02: 1,
         instantaneousIntensity03: 1,
       },
+      limit: request.query.limit ?? 100,
     }
 
     const cursor = linky.find({ createdAt: { $exists: true } }, options)
