d97935431e6f9e61d5029b8ed489aa8c589cd1d4
Wifsimster <wifsimster@gmail.com>
2023-02-26 15:04:29 +0100

    chore: update eslint

--- src/index.js
+++ src/index.js
@@ -8,88 +8,88 @@ const PORT = 3001
 const mongoClient = new MongoClient(mongoUrl)
 
 fastify.register(require('fastify-cors'), {
-  origin: '*',
-  methods: 'GET,POST,PUT,PATCH,DELETE',
+	origin: '*',
+	methods: 'GET,POST,PUT,PATCH,DELETE',
 })
 
 fastify.get('/linky', async (request, reply) => {
-  try {
-    const db = mongoClient.db(config.mongo.db)
-    const linky = db.collection('linky')
-    const cursor = linky.find().sort({ _id: -1 }).limit(1)
-    let results = await cursor.toArray()
-    return results[0]
-  } catch (err) {
-    return err
-  }
+	try {
+		const db = mongoClient.db(config.mongo.db)
+		const linky = db.collection('linky')
+		const cursor = linky.find().sort({ _id: -1 }).limit(1)
+		let results = await cursor.toArray()
+		return results[0]
+	} catch (err) {
+		return err
+	}
 })
 
 fastify.get('/teleinfo', async (request, reply) => {
-  try {
-    const db = mongoClient.db(config.mongo.db)
-    const linky = db.collection('linky')
-    let sort = {}
-    sort[request.query.sort] = request.query.order
-    const options = {
-      sort,
-      projection: {
-        _id: 0,
-        createdAt: 1,
-        apparentPower: 1,
-        instantaneousIntensity01: 1,
-        instantaneousIntensity02: 1,
-        instantaneousIntensity03: 1,
-      },
-      limit: request.query.limit ?? 100,
-    }
+	try {
+		const db = mongoClient.db(config.mongo.db)
+		const linky = db.collection('linky')
+		let sort = {}
+		sort[request.query.sort] = request.query.order
+		const options = {
+			sort,
+			projection: {
+				_id: 0,
+				createdAt: 1,
+				apparentPower: 1,
+				instantaneousIntensity01: 1,
+				instantaneousIntensity02: 1,
+				instantaneousIntensity03: 1,
+			},
+			limit: request.query.limit ?? 100,
+		}
 
-    const cursor = linky
-      .find({ createdAt: { $exists: true } }, options)
-      .allowDiskUse(true)
+		const cursor = linky
+			.find({ createdAt: { $exists: true } }, options)
+			.allowDiskUse(true)
 
-    let results = await cursor.toArray()
+		let results = await cursor.toArray()
 
-    return results
-  } catch (err) {
-    return err
-  }
+		return results
+	} catch (err) {
+		return err
+	}
 })
 
 async function main() {
-  await mongoClient.connect()
-  const db = mongoClient.db(config.mongo.db)
+	await mongoClient.connect()
+	const db = mongoClient.db(config.mongo.db)
 
-  const mqttClient = mqtt.connect(mqttUrl)
+	const mqttClient = mqtt.connect(mqttUrl)
 
-  mqttClient.on('connect', function () {
-    mqttClient.subscribe(config.mqtt.topic, function (err) {
-      if (!err) {
-        console.log(`Subscribe to ${config.mqtt.topic}`)
-      }
-    })
-  })
+	mqttClient.on('connect', function () {
+		mqttClient.subscribe(config.mqtt.topic, function (err) {
+			if (!err) {
+				console.log(`Subscribe to ${config.mqtt.topic}`)
+			}
+		})
+	})
 
-  mqttClient.on('message', (topic, message) => {
-    try {
-      let data = JSON.parse(message.toString())
-      data.createdAt = new Date()
+	mqttClient.on('message', (topic, message) => {
+		try {
+			let data = JSON.parse(message.toString())
+			data.createdAt = new Date()
 
-      db.collection('linky').insertOne(data, function (err, res) {
-        if (err) throw err
-      })
+			db.collection('linky').insertOne(data, function (err, res) {
+				if (err) throw err
+			})
 
-      console.log(data)
-    } catch (err) {
-      console.error(err)
-    }
-  })
+			console.log(data)
+		} catch (err) {
+			console.error(err)
+		}
+	})
 
-  try {
-    await fastify.listen(PORT, '0.0.0.0')
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+	try {
+		await fastify.listen(PORT, '0.0.0.0')
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
 }
 
 main()
